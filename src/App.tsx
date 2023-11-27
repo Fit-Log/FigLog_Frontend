545a048e787962bd659413ada02c7dd89efe51f9
은지 <yeochinlebelomgeole@gmail.com>
2023-11-27 23:17:35 +0900

    feat:: 로그인 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import MainRouter from "./MainRouter";
+import { theme } from "./style/Theme";
+import { GlobalStyle } from "./style/GlobalStyle.style";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <MainRouter />
+    </ThemeProvider>
   );
 }
 
